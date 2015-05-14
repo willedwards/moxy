a6a9a40e6f9655ccff3f194cbcb9a8f97bb9b3bb
willedwards <will.edwards@me.com>
2015-05-14 15:04:48 +0100

    first test passes

--- src/main/java/com/travellazy/package-info.java
+++ src/main/java/com/travellazy/package-info.java
@@ -1,15 +1,20 @@
-@XmlSchema(
-        namespace="http://earth.google.com/kml/2.0",
-        elementFormDefault=XmlNsForm.QUALIFIED,
-        xmlns={
-                @XmlNs(prefix="",
-                        namespaceURI="urn:oasis:names:tc:ciq:xsdschema:xAL:2.0")
-        }
-)
-//@XmlAccessorType(XmlAccessType.FIELD)
-package com.travellazy;
+@XmlSchema(xmlns = {@XmlNs(prefix = "", namespaceURI = "urn:iso:std:iso:20022:tech:xsd:pacs.008.001.01")}) package com.travellazy;
 
-import javax.xml.bind.annotation.*;
+//@XmlSchema(
+//        namespace="http://earth.google.com/kml/2.0",
+//        elementFormDefault=XmlNsForm.QUALIFIED,
+//        xmlns={
+//                @XmlNs(prefix = "ns",
+//                        namespaceURI="urn:oasis:names:tc:ciq:xsdschema:xAL:2.0")
+//        }
+//)
+//@XmlAccessorType(XmlAccessType.FIELD)
+
+import javax.xml.bind.annotation.XmlNs;
+import javax.xml.bind.annotation.XmlSchema;
+
+
+
 
  /*
  This package-info.java file defines package level properties that are used during the Jaxb marshalling of classes
